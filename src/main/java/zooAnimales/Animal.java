9e82a91158c8f9e60c8a5bf630cf3723a5fec809
nsalinas350 <nsalinas@unal.edu.co>
2021-11-24 01:19:10 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -65,7 +65,7 @@ public class Animal {
 				"Aves: "+ Ave.getListado().size()+"\n"+
 				"Reptiles: "+ Reptil.getListado().size()+"\n"+
 				"Peces: "+ Pez.getListado().size()+"\n"+
-				"Anfibios: "+ Anfibio.getListado().size()+"\n";
+				"Anfibios: "+ Anfibio.getListado().size();
 	}
 	
 }
