1dad76708ff56ca252db6f7f515bf60692ba6777
nsalinas350 <nsalinas@unal.edu.co>
2021-11-23 21:02:53 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,5 +1,37 @@
 package zooAnimales;
 
 public class Animal {
+	private String nombre;
+	private int edad;
+	private String habitat;
+	private String genero;
+	
+	private int totalAnimales;
+	private Zona zona;
+	
+	public Animal() {	
+	}
+	public Animal(String nombre,int edad,String habitat,String genero){
+		this.nombre = nombre;
+		this.edad = edad;
+		this.habitat = habitat;
+		this.genero = genero;
+	}
+	
+	public String getNombre(){
+		return this.nombre;
+	}
+	
+	public String getEdad(){
+		return this.edad;
+	}
+	
+	public String getHabitat(){
+		return this.habitat;
+	}
+	
+	public String getGenero(){
+		return this.genero;
+	}
 
 }
