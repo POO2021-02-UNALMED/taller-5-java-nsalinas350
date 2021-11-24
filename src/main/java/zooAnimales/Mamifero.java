ee2e2983d44af05847164ef614f261799d7b8450
nsalinas350 <nsalinas@unal.edu.co>
2021-11-23 21:10:27 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,5 +1,28 @@
 package zooAnimales;
 
 public class Mamifero extends Animal {
+	private static Mamifero[] listado;
+	public int caballos;
+	public int leones;
+	private boolean pelaje;
+	private int patas;
+	
+	public Mamifero() {}
+	public Mamifero(String nombre,int edad,String habitat,String genero,boolean pelaje,int patas) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.habitat = habitat;
+		this.genero = genero;
+		this.pelaje = pelaje;
+		this.patas = patas;
+	}
+	
+	public String isPelaje(){
+		return this.pelaje;
+	}
+	
+	public String getPatas(){
+		return this.patas;
+	}
 
 }
